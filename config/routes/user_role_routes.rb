ccbbe046aa270c40e0dc888aa40706a9852ff380
Bhatt-Om <122429893+Bhatt-Om@users.noreply.github.com>
2024-03-20 17:40:10 +0530

    fix user role

--- config/routes/user_role_routes.rb
+++
@@ -1,1 +0,0 @@
-resources :user_roles, only: %i[index create update]
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,13 @@ if Doorkeeper::Application.count.zero?
     scopes: ''
   )
 end
+
+if !Role.where(role_name: 'admin').any?
+  Role.create!(role_name: 'admin')
+  Role.create!(role_name: 'volunteer')
+  User.create!(email: 'admin@email.com', password: '123456', role_id: Role.find_by(role_name: 'admin').id)
+end
+
+if Rails.env.development? && !Task.any?
+  5.times { |num| Task.create!(event_name: "Event #{num}",other_instruction: 'thank you for joining', event_location: 'location', google_link: 'link', date: Date.today.strftime("%d/%m/%Y"), time: Time.now.strftime('%H:%M:%S'), points: '100')}
+end
